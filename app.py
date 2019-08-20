bb6e49c40fe06949a9cd489931fb5764ab59c289
serj <serj.krukov@gmail.com>
2019-08-20 13:43:50 +0300

    crud with gino

--- app.py
+++ app.py
@@ -1,40 +1,72 @@
 from sanic import Sanic
 from sanic.response import json
+from sanic import response
 from sanic.exceptions import abort
 from blueprint import bp
 from gino.ext.sanic import Gino
+import asyncpg
 
-app = Sanic()
-app.config.DB_HOST = 'ec2-54-228-243-238.eu-west-1.compute.amazonaws.com'
-app.config.DB_DATABASE = 'd5vbjksh0aovib'
-app.config.DB_USER = 'cptwarcybkjxed'
-app.config.DB_PASSWORD = '84f2039c559006c9165f9aea1cc4d4766f85be6e9e51362243fa157a44c1692c'
-app.config.DB_SSL = 'require'
+
+app = Sanic(__name__)
+app.config.from_pyfile('./.env')
 db = Gino()
 db.init_app(app)
 app.blueprint(bp)
 
 
 class User(db.Model):
-    __tablename__ = 'modelusers'
+    __tablename__ = 'usersmodels'
 
     id = db.Column(db.BigInteger(), primary_key=True)
-    nickname = db.Column(db.Unicode())
+    data = db.Column(db.JSON())
 
     def __repr__(self):
-        return '{}<{}>'.format(self.nickname, self.id)
+        return '{}<{}>'.format(self.data, self.id)
 
 @app.post('/users')
 async def create_user(request):
-    user = await User.create(nickname='fantix')
+    try:
+        user = await User.create(data = request.json)
+        return json({'id':user.id})
+    except asyncpg.exceptions.UniqueViolationError:
+        return json({'status': 'failed', 'reason': 'email already exists'})
+    except asyncpg.exceptions.CheckViolationError:
+        return json({'status': 'failed', 'reason': 'email not valid'})
 
-@app.route("/users/<user_id>")
+@app.get("/users/<user_id>")
 async def get_user(request, user_id):
     if not user_id.isdigit():
         abort(400, 'invalid user id')
     user = await User.get_or_404(int(user_id))
-    return json({'name': user.nickname})
+    user.data['id'] = user.id
+    return json(user.data)
 
+@app.get("/users")
+async def get_all_users(request):
+    all_users = await User.query.gino.all()
+    users_list = []
+    for user in all_users:
+        user.data['id'] = user.id
+        users_list.append(user.data)
+    return response.json(users_list)
+@app.patch("/users/<user_id>")
+async def update_user(request, user_id):
+    if not user_id.isdigit():
+        abort(400, 'invalid user id')
+    user = await User.get_or_404(int(user_id))
+    new_data = []
+    for key, value in request.json.items():
+        user.data[key] = value
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0.', port=9000, debug=True)
+    await user.update(data = user.data).apply()
+    return json(user.data)
+
+@app.delete("/users/<user_id>")
+async def del_user(request, user_id):
+    if not user_id.isdigit():
+        abort(400, 'invalid user id')
+    user = await User.get_or_404(int(user_id))
+    await user.delete()
+    return json({"id": user_id, "status": "deleted"})
+# if __name__ == '__main__':
+app.run(host='0.0.0.0', port=9000, debug=True)
